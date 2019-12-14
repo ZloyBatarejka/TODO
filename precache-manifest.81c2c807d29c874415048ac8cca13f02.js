7f6cf7a615b18a0b6abf16783fc3f1ad33dd11ba
Yakovlev Ruslanz <57760502+ZloyBatarejka@users.noreply.github.com>
2019-12-14 17:24:59 +0300

    Updates

---
+++ precache-manifest.81c2c807d29c874415048ac8cca13f02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e200890613c5f7970c4f564b467f2ff6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "93153ae40367b48fab0a",
+    "url": "/todolist/static/css/main.a417796c.chunk.css"
+  },
+  {
+    "revision": "8c7f36ffc26f66a4b7af",
+    "url": "/todolist/static/js/2.8fcae5d8.chunk.js"
+  },
+  {
+    "revision": "93153ae40367b48fab0a",
+    "url": "/todolist/static/js/main.962121e0.chunk.js"
+  },
+  {
+    "revision": "4fee5031c8de36fe5f75",
+    "url": "/todolist/static/js/runtime-main.964b1b52.js"
+  }
+]);
